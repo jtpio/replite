f0d59afe226cbaaf564e97ac292b57b160d4e47d
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-01 20:11:30 +0100

    Read code from query string parameters

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,30 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { IConsoleTracker } from '@jupyterlab/console';
+
 /**
  * Initialization data for the replite extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'replite:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension replite is activated!');
+  optional: [IConsoleTracker],
+  activate: (app: JupyterFrontEnd, tracker: IConsoleTracker) => {
+    const search = window.location.search;
+    const urlParams = new URLSearchParams(search);
+    const code = urlParams.getAll('code');
+    if (!code) {
+      return;
+    }
+    tracker.widgetAdded.connect((_, widget) => {
+      const { console } = widget;
+      console.promptCellCreated.connect(() => {
+        if (console.promptCell) {
+          console.promptCell.model.value.text = code.join('\n');
+        }
+      });
+    });
   }
 };
 
