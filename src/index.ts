f6afb63b43d6d659c639ca05025b1cc5f604794a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-02 09:56:15 +0100

    Add powered by banner

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import { Dialog } from '@jupyterlab/apputils';
 
 import { IConsoleTracker } from '@jupyterlab/console';
 
+import { Panel, PanelLayout, Widget } from '@lumino/widgets';
+
+import { liteIcon } from '@jupyterlite/ui-components';
+
 /**
  * Initialization data for the replite extension.
  */
@@ -26,16 +30,40 @@ const plugin: JupyterFrontEndPlugin<void> = {
     tracker.widgetAdded.connect((_, widget) => {
       const { console } = widget;
 
-      // pre-populate code when the console is created
-      if (code) {
-        const populate = () => {
-          if (console.promptCell) {
+      const populate = () => {
+        if (console.promptCell) {
+          // pre-populate code when the console is created
+          if (code) {
             console.promptCell.model.value.text = code.join('\n');
-            console.promptCellCreated.disconnect(populate);
           }
-        };
-        console.promptCellCreated.connect(populate);
-      }
+
+          const wrapper = new Panel();
+          wrapper.addClass('jp-PoweredBy');
+
+          const node = document.createElement('a');
+          node.textContent = 'Powered by JupyterLite';
+          node.href = 'https://github.com/jupyterlite/jupyterlite';
+          node.target = '_blank';
+          node.rel = 'noopener noreferrer';
+          const poweredBy = new Widget({ node });
+          const icon = new Widget();
+          liteIcon.element({
+            container: icon.node,
+            elementPosition: 'center',
+            margin: '2px 2px 2px 8px',
+            height: 'auto',
+            width: '16px'
+          });
+
+          const layout = console.layout as PanelLayout;
+          wrapper.addWidget(poweredBy);
+          wrapper.addWidget(icon);
+          layout.addWidget(wrapper);
+
+          console.promptCellCreated.disconnect(populate);
+        }
+      };
+      console.promptCellCreated.connect(populate);
 
       // hide the first select kernel dialog if a kernel is specified
       // TODO: support specifying kernel preference in upstream RetroLab
